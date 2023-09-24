3b5324c397f2ade3b7189efdc03c3face258afe3
Md Rony <mdronyrsk01724@gmail.com>
2023-09-24 23:33:10 +0600

    travel list desing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
-  return (
-    <div>
-      hiii
-    </div>
-  )
-}
+  const initialItem = [
+    { id: 1, description: "passport", qunatity: 2, packed: false },
+    { id: 2, description: "socks", qunatity: 12, packed: false },
+  ];
 
-export default App
+  const Logo = () => {
+    return <h1>Far Way</h1>;
+  };
+
+  const Form = () => {
+    return <div className="add-form">What do you need for your trip?</div>;
+  };
+
+  const Pakagelist = () => {
+    return (
+      <div className="list">
+        <ul>
+          {initialItem.map((item, i) => (
+            <Item key={i} item={item} />
+          ))}
+        </ul>
+      </div>
+    );
+  };
+
+  const Status = () => {
+    return (
+      <footer className="stats">
+        <em>You Have X items in Your List, and you already packed X</em>
+      </footer>
+    );
+  };
+
+  const Item = ({ item }) => {
+    return <li>{item.description}</li>;
+  };
+
+  return (
+    <>
+      <div className="app">
+        <Logo />
+        <Form />
+        <Pakagelist />
+        <Status />
+      </div>
+    </>
+  );
+};
+
+export default App;
