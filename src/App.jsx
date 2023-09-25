a1b0b90ba40c119116b1740abc9e83b6eea1d496
Md Rony <mdronyrsk01724@gmail.com>
2023-09-25 23:19:57 +0600

    dynamic from and select

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
   const initialItem = [
     { id: 1, description: "passport", qunatity: 2, packed: false },
-    { id: 2, description: "socks", qunatity: 12, packed: false },
+    { id: 2, description: "socks", qunatity: 12, packed: true },
+    { id: 2, description: "charger", qunatity: 1, packed: false },
   ];
 
   const Logo = () => {
@@ -11,7 +12,42 @@ const App = () => {
   };
 
   const Form = () => {
-    return <div className="add-form">What do you need for your trip?</div>;
+    const [description, setDescription] = useState("");
+    const [select, setSelect] = useState(1);
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+    };
+
+    const handleChange = (e) => {
+      setDescription(e.target.value);
+    };
+
+    const handleSelect = (e) => {
+      setSelect(Number(e.target.value));
+    };
+
+    // console.log(select);
+
+    return (
+      <form className="add-form" onSubmit={handleSubmit}>
+        <h3>What do you need for your trip?</h3>
+        <select onChange={handleSelect} value={select}>
+          {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+            <option value={num} key={num}>
+              {num}
+            </option>
+          ))}
+        </select>
+        <input
+          type="text"
+          placeholder="Item..."
+          value={description}
+          onChange={handleChange}
+        />
+        <button>Add</button>
+      </form>
+    );
   };
 
   const Pakagelist = () => {
@@ -35,7 +71,14 @@ const App = () => {
   };
 
   const Item = ({ item }) => {
-    return <li>{item.description}</li>;
+    return (
+      <li>
+        <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+          {item.qunatity} {item.description}
+        </span>
+        <button className="button">&times;</button>
+      </li>
+    );
   };
 
   return (
